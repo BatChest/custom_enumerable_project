bb49a97c74ba4bf0a3076e623fe5788b76833104
luisb <barneydino55@gmail.com>
2025-04-23 10:56:14 -0700

    Finished two enumerables

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,15 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index(&block)
+    result = []
+    position = 0
+    self.my_each do |item|
+      result << block.call(item, position)
+      position += 1
+    end
+  end
+
+
 end
 
 # You will first have to define my_each
@@ -8,4 +18,10 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each(&block)
+    for i in 0...self.length
+      block.call(self[i])
+    end
+    return self
+  end
 end
